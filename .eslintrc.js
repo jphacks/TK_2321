a449a036c67bd19a8108c645b7c7b5c906914506
konjikun <s1f102200378@iniad.org>
2022-11-19 22:38:13 +0900

    chakraとの折り合い

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,44 @@
 module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
-    es2021: true
+    node: true,
+    es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: [
-    'react'
-  ],
+  ecmaFeatures: {
+    impliedStrict: true, //常にStrictMode
+  },
   rules: {
-  }
+    'react/prop-types': 'off',
+    'react/self-closing-comp': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'prefer-template': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-dupe-args': ['error'],
+    'no-dupe-keys': ['error'],
+    'no-unreachable': ['error'],
+  },
 }
