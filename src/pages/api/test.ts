48a11b6857b0af802d3a57bb9cfad61c3315fb04
konjikun <shintaniallen@gmail.com>
2023-02-05 15:13:35 +0900

    lintを通す

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 type TestRes = {
   title: string
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import React from 'react'
 import styled from 'styled-components'
 import Button from '@mui/material/Button'
 
