21d60fd8000ef1375b930fc18d1407f22c1d7d95
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2023-10-30 17:23:48 +0900

    add get-all-Yugioh-Card

--- src/services/pokemon/get-all-cards.ts
+++ src/services/pokemon/get-all-cards.ts
@@ -5,7 +5,7 @@ import { fetcher } from "@/utils";
  * Pokemon API (get one Pokemon Card)
  * @returns All Cards
  */
-const getAllPokemonCard = async (): Promise<[PokemonCardType]> => {
+const getAllPokemonCard = async (): Promise<PokemonCardType[]> => {
   return await fetcher({
     resource: `${process.env.NEXT_PUBLIC_POKEMONTCG_API_URL}/cards`
   });
