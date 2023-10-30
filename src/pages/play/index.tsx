f3944dde6e7c9a16259eab2647d52db6600364e8
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2023-10-30 12:48:35 +0900

    delete Card StoryBook Component

--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PokemonCard from '@/components/atoms/PokemonCard'
+import AppLogo from '@/components/atoms/AppLogo'
 
 const Test = () => {
   return (
     <div>
       <PokemonCard id={89} />
+      <AppLogo />
     </div>
   )
 }
