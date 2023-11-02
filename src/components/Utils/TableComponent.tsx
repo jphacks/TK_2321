f76c9fa5e44f5c68d56cca76bff790cc626fec7f
konjikun <shintaniallen@gmail.com>
2023-11-02 15:45:31 +0900

    clean pockemon field

---
+++ src/components/Utils/TableComponent.tsx
@@ -0,0 +1,107 @@
+// eslint-disable-next-line
+import React from 'react'
+import { useState } from 'react'
+
+interface ImageInfo {
+  url: string
+  // その他の画像情報があればここに追加します
+}
+
+interface TableComponentProps {
+  images?: ImageInfo[]
+}
+
+const TableComponent: React.FC<TableComponentProps> = ({ images = [] }) => {
+  const rows = 11
+  const cols = 12
+  const [selectedCell, setSelectedCell] = useState<string | null>(null)
+
+  const [specifiedImageUrls, setSpecifiedImageUrls] = useState<{
+    [key: string]: string | undefined
+  }>({
+    // 初期状態での特定のセルの画像
+    '5-5': './1.jpeg',
+  })
+
+  // 各セルをクリックしたときのハンドラーです
+  const handleCellClick = (rowIndex: number, colIndex: number) => {
+    setSelectedCell(`${rowIndex}-${colIndex}`)
+    // ここに各セルに対する処理を追加します
+  }
+
+  // トラッシュボタンがクリックされたときのハンドラーです
+  const handleTrashClick = () => {
+    // トラッシュに関する処理を追加します
+    setSelectedCell(null)
+  }
+
+  // 山札からカードを引くときのハンドラーです
+  const handleDrawClick = () => {
+    // 山札からカードを引く処理を追加します
+  }
+
+  return (
+    <div style={{ overflow: 'auto', width: '100%', height: '100vh' }}>
+      <table>
+        <tbody>
+          {Array.from({ length: rows }).map((_, rowIndex) => (
+            <tr key={rowIndex}>
+              {Array.from({ length: cols }).map((_, colIndex) => {
+                // 特定のフィールドに基づく条件をここに配置
+                const isBenchField =
+                  rowIndex === 9 && colIndex >= 4 && colIndex <= 8
+                const isBattleField = rowIndex === 7 && colIndex === 6
+                const isTrashField = rowIndex === 9 && colIndex === 11
+                const isDeckField = rowIndex === 7 && colIndex === 11
+                const isSideField =
+                  rowIndex >= 7 && rowIndex <= 9 && colIndex === 1
+                //
+                const isCalledBenchField =
+                  rowIndex === 1 && colIndex >= 4 && colIndex <= 8
+                const isCalledBattleField = rowIndex === 3 && colIndex === 6
+                const isCalledTrashField = rowIndex === 1 && colIndex === 1
+                const isCalledDeckField = rowIndex === 3 && colIndex === 1
+                const isCalledSideField =
+                  rowIndex >= 1 && rowIndex <= 3 && colIndex === 11
+
+                return (
+                  <td
+                    key={colIndex}
+                    onClick={() => handleCellClick(rowIndex, colIndex)}
+                    style={{
+                      width: '80px',
+                      height: '120px',
+                      padding: '5px',
+                      border:
+                        isBenchField ||
+                        isBattleField ||
+                        isTrashField ||
+                        isDeckField ||
+                        isSideField ||
+                        isCalledBenchField ||
+                        isCalledBattleField ||
+                        isCalledTrashField ||
+                        isCalledDeckField ||
+                        isCalledSideField
+                          ? '1px solid black'
+                          : 'none',
+                      boxSizing: 'border-box',
+                      position: 'relative',
+                      backgroundColor: isBattleField
+                        ? 'lightblue'
+                        : 'transparent',
+                    }}
+                  >
+                    {/* ここに各セルのコンテンツを追加します */}
+                  </td>
+                )
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default TableComponent
---
+++ src/pages/call.tsx
@@ -0,0 +1,119 @@
+import type { MediaConnection } from 'peerjs'
+// eslint-disable-next-line
+import React from 'react'
+import usePeerManager from '../usePeerManager'
+import TableComponent from '@/components/Utils/TableComponent'
+import { useEffect, useRef, useState } from 'react'
+
+const Call: React.FC = () => {
+  const { myId, callPeer, onCall } = usePeerManager()
+  const [peerIdToCall, setPeerIdToCall] = useState<string | null>(null)
+  const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null)
+  const [isConnected, setIsConnected] = useState<boolean>(false)
+  const [isCaller, setIsCaller] = useState<boolean>(false) // 通話を開始するユーザーを識別するためのstate
+  const remoteVideoRef = useRef<HTMLVideoElement>(null)
+  const [pokemonButtonClicked, setPokemonButtonClicked] = useState(false) // ポケモンボタンがクリックされたかどうかの状態
+
+  const handlePokemonClick = () => {
+    setPokemonButtonClicked(true) // ポケモンボタンがクリックされた状態に更新
+    handleCallPeer()
+  }
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((stream) => {
+        onCall((call: MediaConnection) => {
+          call.answer(stream)
+          call.on('stream', (remoteStream: MediaStream) => {
+            setRemoteStream(remoteStream)
+            setIsConnected(true)
+          })
+        })
+      })
+      .catch((error) => {
+        console.error('Error accessing media devices:', error)
+      })
+  }, [onCall])
+
+  useEffect(() => {
+    if (remoteVideoRef.current && remoteStream) {
+      remoteVideoRef.current.srcObject = remoteStream
+    }
+  }, [remoteStream])
+
+  const handleCallPeer = () => {
+    setIsCaller(true) // 通話を開始するユーザーとして識別
+    if (peerIdToCall) {
+      navigator.mediaDevices
+        .getUserMedia({ video: false, audio: true })
+        .then((stream) => {
+          const call = callPeer(peerIdToCall, stream)
+          if (call) {
+            call.on('stream', (remoteStream: MediaStream) => {
+              setRemoteStream(remoteStream)
+              setIsConnected(true)
+            })
+          }
+        })
+    }
+  }
+
+  return (
+    <div style={{ display: 'flex', height: '100vh' }}>
+      {/* Sidebar goes here */}
+      <div style={{ width: '300px', background: '#f0f0f0' }}>
+        {/* Sidebar content here */}
+      </div>
+
+      {/* Main content area */}
+      <div style={{ flexGrow: 1, position: 'relative' }}>
+        {!isConnected && (
+          <>
+            <h1>My ID: {myId}</h1>
+            <input
+              type="text"
+              placeholder="Enter peer ID"
+              onChange={(e) => setPeerIdToCall(e.target.value)}
+            />
+            <button onClick={handlePokemonClick}>ポケモン</button>
+            <button onClick={handleCallPeer}>遊戯王</button>
+          </>
+        )}
+        {isConnected && (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              zIndex: 1,
+              transform: isCaller ? 'rotate(180deg)' : 'none',
+              overflow: 'auto',
+            }}
+          >
+            <TableComponent />
+          </div>
+        )}
+        <video
+          ref={remoteVideoRef}
+          playsInline
+          autoPlay
+          style={{
+            pointerEvents: 'none',
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+          }} // Adjusted video position
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Call
